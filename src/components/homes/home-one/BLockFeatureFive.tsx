e56d173e6ee9a486956431a39b54614ef08b7ecf
majitoven <mariajose_v96@hotmail.com>
2024-05-16 19:46:20 -0300

    added arrow

--- src/components/homes/home-one/BLockFeatureFive.tsx
+++ src/components/homes/home-one/BLockFeatureFive.tsx
@@ -24,7 +24,7 @@ const BLockFeatureFive = ({ style }: any) => {
                      {feature_data.filter((items) => items.page === "home_1_feature_3").map((item) => (
                         <div key={item.id} className="col-lg-6 col-sm-6">
                            <div className={`card-style-one text-center wow fadeInUp ${item.class_name}`}>
-                              <Image src={item.icon ? item.icon : ""} alt="" className="lazy-img m-auto icon" />
+                              <Image src={item.icon ? item.icon : ""} alt="" style={{maxHeight: '150px'}} className="lazy-img m-auto icon" />
                               <h5 className="mt-50 lg-mt-30 mb-15">{item.title}</h5>
                               <p className="pe-xxl-4 ps-xxl-4">{item.desc}</p>
                            </div>
