a2ac3b53811782c4fc8b4ba5a259cc9660c45732
majitoven <mariajose_v96@hotmail.com>
2024-07-05 15:53:42 -0300

    added languages

--- src/layouts/headers/Menu/NavMenu.tsx
+++ src/layouts/headers/Menu/NavMenu.tsx
@@ -6,11 +6,14 @@ import Image from "next/image";
 import { useState } from "react";
 
 import logo from "@/assets/images/logo/logo_01.svg";
+import spainFlag from "@/assets/images/icon/spain.png";
+import ukFlag from "@/assets/images/icon/united-kingdom.png";
 
 const NavMenu = () => {
     const pathname = usePathname();
     const currentRoute = usePathname();
     const [navTitle, setNavTitle] = useState("");
+    const [language, setLanguage] = useState("en");
 
     const isMenuItemActive = (menuLink: string) => {
         return currentRoute === menuLink;
@@ -20,7 +23,6 @@ const NavMenu = () => {
         return currentRoute === subMenuLink;
     };
 
-    //openMobileMenu
     const openMobileMenu = (menu: any) => {
         if (navTitle === menu) {
             setNavTitle("");
@@ -29,48 +31,82 @@ const NavMenu = () => {
         }
     };
 
+    const changeLanguage = (lang: string) => {
+        setLanguage(lang);
+        // Add logic to handle language change, e.g., reload page or update content
+    };
+
+
     return (
-        <ul className="navbar-nav align-items-lg-center">
-            <li className="d-block d-lg-none"><div className="logo"><Link href="/" className="d-block"><Image src={logo} alt="" /></Link></div></li>
-            {menu_data.map((menu: any, index) => (
-                <li key={menu.id} className={`nav-item dropdown ${menu.class_name} ${index == 0 && 'dashboard-menu'}`}>
-                    <Link href={menu.link} className={`nav-link ${menu.has_dropdown && 'dropdown-toggle'} ${pathname === menu.link ? 'active' : ''}
-                     ${navTitle === menu.title ? "show" : ""}`} onClick={() => openMobileMenu(menu.title)}>
-                        {menu.title}
-                    </Link>
-                    {menu.has_dropdown && (
-                        <>
-                            <ul className={`dropdown-menu ${navTitle === menu.title ? "show" : ""}`}>
-                                {menu.sub_menus && menu.sub_menus.map((sub_m: any, i: any) => (
-                                    <li key={i}>
-                                        <Link href={sub_m.link} className={`dropdown-item ${pathname === sub_m.link ? 'active' : ''}`}>
-                                            <span>{sub_m.title}</span>
-                                        </Link>
-                                    </li>
-                                ))}
-                                {menu.menu_column && (
-                                    <li className="row gx-1">
-                                        {menu.menu_column.map((item: any) => (
-                                            <div key={item.id} className="col-lg-4">
-                                                <div className="menu-column">
-                                                    <h6 className="mega-menu-title">{item.mega_title}</h6>
-                                                    <ul className="style-none mega-dropdown-list">
-                                                        {item.mega_menus.map((mega_m: any, i: any) => (
-                                                            <li key={i}><Link href={mega_m.link} className={`dropdown-item ${pathname === mega_m.link ? 'active' : ''}`}><span>{mega_m.title}</span></Link></li>))}
-                                                    </ul>
-                                                </div>
-                                            </div>
-                                        ))}
-                                    </li>
-                                )}
-                            </ul>
-                        </>
-                    )}
+        <div className="navbar-container">
+            <ul className="navbar-nav align-items-lg-center">
+                <li className="d-block d-lg-none">
+                    <div className="logo">
+                        <Link href="/" className="d-block">
+                            <Image src={logo} alt="" />
+                        </Link>
+                    </div>
                 </li>
-            ))}
-        </ul>
+                {menu_data.map((menu: any, index) => (
+                    <li key={menu.id} className={`nav-item dropdown ${menu.class_name} ${index == 0 && 'dashboard-menu'}`}>
+                        <Link href={menu.link} className={`nav-link ${menu.has_dropdown && 'dropdown-toggle'} ${pathname === menu.link ? 'active' : ''}
+                     ${navTitle === menu.title ? "show" : ""}`} onClick={() => openMobileMenu(menu.title)}>
+                            {menu.title}
+                        </Link>
+                        {menu.has_dropdown && (
+                            <>
+                                <ul className={`dropdown-menu ${navTitle === menu.title ? "show" : ""}`}>
+                                    {menu.sub_menus && menu.sub_menus.map((sub_m: any, i: any) => (
+                                        <li key={i}>
+                                            <Link href={sub_m.link} className={`dropdown-item ${pathname === sub_m.link ? 'active' : ''}`}>
+                                                <span>{sub_m.title}</span>
+                                            </Link>
+                                        </li>
+                                    ))}
+                                    {menu.menu_column && (
+                                        <li className="row gx-1">
+                                            {menu.menu_column.map((item: any) => (
+                                                <div key={item.id} className="col-lg-4">
+                                                    <div className="menu-column">
+                                                        <h6 className="mega-menu-title">{item.mega_title}</h6>
+                                                        <ul className="style-none mega-dropdown-list">
+                                                            {item.mega_menus.map((mega_m: any, i: any) => (
+                                                                <li key={i}><Link href={mega_m.link} className={`dropdown-item ${pathname === mega_m.link ? 'active' : ''}`}><span>{mega_m.title}</span></Link></li>))}
+                                                        </ul>
+                                                    </div>
+                                                </div>
+                                            ))}
+                                        </li>
+                                    )}
+                                </ul>
+                            </>
+                        )}
+                    </li>
+                ))}
+            </ul>
+            <div className="language-switcher">
+                <div className="dropdown">
+                    <a className="nav-link dropdown-toggle" id="languageDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false" style={{
+                        display: 'flex',
+                        alignItems: 'center'
+                    }}>
+                        <Image src={language === "en" ? ukFlag : spainFlag} alt="language flag" width={24} height={24} />
+                    </a>
+                    <ul className="dropdown-menu dropdown-menu-end" aria-labelledby="languageDropdown">
+                        <li><a className="dropdown-item" onClick={() => changeLanguage("en")} style={{
+                            display: 'flex',
+                            alignItems: 'center'
+                        }}><Image src={ukFlag} alt="English" width={24} height={24} style={{ marginRight: '10px' }} /> English</a></li>
+                        <li><a className="dropdown-item" onClick={() => changeLanguage("es")} style={{
+                            display: 'flex',
+                            alignItems: 'center'
+                        }}><Image src={spainFlag} alt="Spanish" width={24} height={24} style={{ marginRight: '10px' }} /> Español</a></li>
+                    </ul>
+                </div>
+            </div>
+
+        </div>
     );
 };
 
 export default NavMenu;
-
