6b36566ecb6e54db560624586f4ead106c8631df
majitoven <mariajose_v96@hotmail.com>
2024-07-10 11:25:33 -0300

    fixed zindex

--- src/components/homes/home-three/HeroBanner.tsx
+++ src/components/homes/home-three/HeroBanner.tsx
@@ -19,7 +19,7 @@ const setting = {
 
 const HeroBanner = () => {
    return (
-      <div className="hero-banner-three position-relative  pt-130 lg-pt-100 pb-170 xl-pb-130 lg-pb-100">
+      <div className="hero-banner-three position-relative pt-130 lg-pt-100 pb-170 xl-pb-130 lg-pb-100">
          <Slider {...setting} className="hero-slider-one m0">
             <div className="item m0"><div className="hero-img" style={{ backgroundImage: `url(/assets/images/media/A1.jpg)` }}></div></div>
             <div className="item m0"><div className="hero-img" style={{ backgroundImage: `url(/assets/images/media/A2.jpg)` }}></div></div>
