531a3822821ddd7c86303d082c857c46b62cc1b3
majitoven <mariajose_v96@hotmail.com>
2024-07-09 22:06:30 -0300

    Update route.js

--- src/app/api/sendEmail/route.js
+++ src/app/api/sendEmail/route.js
@@ -36,13 +36,13 @@ export async function POST(request) {
       from: process.env.EMAIL_USER,
       to: 'mventura@loopstudio.dev',
       subject: 'Web Moviit: Formulario de contacto',
-      text: `Name: ${name}\nEmail: ${email}\nPhone: ${phone}\nMessage: ${message}`,
+      text: `Nombre: ${name}\nEmail: ${email}\nTelefono: ${phone}\nMensaje: ${message}`,
     };
 
     await transporter.sendMail(mailOptions);
-    return NextResponse.json({ message: 'Email sent successfully' }, { status: 200 });
+    return NextResponse.json({ message: 'Mensaje enviado exitosamente' }, { status: 200 });
   } catch (error) {
-    console.error('Error sending email:', error);
-    return NextResponse.json({ error: 'Error sending email' }, { status: 500 });
+    console.error('Error enviando email:', error);
+    return NextResponse.json({ error: 'Error enviando email' }, { status: 500 });
   }
 }
