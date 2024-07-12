e2a82f72b3c109aa05645458fae550fdc60a0bf1
majitoven <mariajose_v96@hotmail.com>
2024-07-12 20:35:00 -0300

    hide yotubue thing

--- src/components/ListingDetails/listing-details-5/VideoTour.tsx
+++ src/components/ListingDetails/listing-details-5/VideoTour.tsx
@@ -1,43 +1,53 @@
-"use client"
+"use client";
 import { useState } from "react";
 import VideoPopup from "@/modals/VideoPopup";
-import Image from "next/image"
+import Image from "next/image";
 import videoImg from "@/assets/images/listing/img_47.jpg";
 
 const VideoTour = () => {
+  const [isVideoOpen, setIsVideoOpen] = useState(false);
 
-   const [isVideoOpen, setIsVideoOpen] = useState(false);
-
-   return (
-      <>
-         <div className="accordion-item">
-            <h2 className="accordion-header">
-               <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseFourA" aria-expanded="true" aria-controls="collapseFourA">
-                  Video Tour
-               </button>
-            </h2>
-            <div id="collapseFourA" className="accordion-collapse collapse">
-               <div className="accordion-body">
-                  <div className="property-video-tour">
-                     <div className="position-relative image-bg overflow-hidden z-1">
-                        <Image src={videoImg} alt="" className="lazy-img w-100" />
-                        <a onClick={() => setIsVideoOpen(true)} style={{ cursor: "pointer" }} className="video-icon tran3s rounded-circle d-flex align-items-center justify-content-center" data-fancybox >
-                           <i className="fa-thin fa-play"></i>
-                        </a>
-                     </div>
-                  </div>
-               </div>
+  return (
+    <>
+      <div className="accordion-item">
+        <h2 className="accordion-header">
+          <button
+            className="accordion-button collapsed"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#collapseFourA"
+            aria-expanded="true"
+            aria-controls="collapseFourA"
+          >
+            Video Tour
+          </button>
+        </h2>
+        <div id="collapseFourA" className="accordion-collapse collapse">
+          <div className="accordion-body">
+            <div className="property-video-tour">
+              <div className="position-relative image-bg overflow-hidden z-1">
+                <Image src={videoImg} alt="" className="lazy-img w-100" />
+                <a
+                  onClick={() => setIsVideoOpen(true)}
+                  style={{ cursor: "pointer" }}
+                  className="video-icon tran3s rounded-circle d-flex align-items-center justify-content-center"
+                >
+                  <i className="fa-thin fa-play"></i>
+                </a>
+              </div>
             </div>
-         </div>
-         {/* video modal start */}
-         <VideoPopup
-            isVideoOpen={isVideoOpen}
-            setIsVideoOpen={setIsVideoOpen}
-            videoId={"H0h49xxJU0U"}
-         />
-         {/* video modal end */}
-      </>
-   )
-}
+          </div>
+        </div>
+      </div>
+      {/* video modal start */}
+      <VideoPopup
+        isVideoOpen={isVideoOpen}
+        setIsVideoOpen={setIsVideoOpen}
+        videoId={"H0h49xxJU0U"} // Update this to your video ID
+      />
+      {/* video modal end */}
+    </>
+  );
+};
 
-export default VideoTour
+export default VideoTour;
--- src/modals/VideoPopup.tsx
+++ src/modals/VideoPopup.tsx
@@ -1,16 +1,22 @@
 import ModalVideo from "react-modal-video";
+import "react-modal-video/scss/modal-video.scss";  // Ensure this import is included
 
-const VideoPopup = ({
-  isVideoOpen,
-  setIsVideoOpen,
-  videoId = "H0h49xxJU0U", 
-  
-}:any ) => {
+const VideoPopup = ({ isVideoOpen, setIsVideoOpen, videoId = "H0h49xxJU0U" }) => {
   return (
     <>
       <ModalVideo
         channel="youtube"
-        autoplay={1}
+        youtube={{
+          autoplay: 1,
+          modestbranding: 1,
+          rel: 0,
+          showinfo: 0,
+          controls: 0,
+          disablekb: 1,
+          fs: 0,
+          iv_load_policy: 3,
+          cc_load_policy: 0,
+        }}
         isOpen={isVideoOpen}
         videoId={videoId}
         onClose={() => setIsVideoOpen(false)}
@@ -20,4 +26,3 @@ const VideoPopup = ({
 };
 
 export default VideoPopup;
-
