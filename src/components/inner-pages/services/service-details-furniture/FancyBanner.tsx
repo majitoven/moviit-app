cb5605746d5078c20804a77aaaf0c5e8f1a42ba6
majitoven <mariajose_v96@hotmail.com>
2024-07-12 19:32:24 -0300

    try head

--- src/components/inner-pages/services/service-details-furniture/FancyBanner.tsx
+++ src/components/inner-pages/services/service-details-furniture/FancyBanner.tsx
@@ -1,20 +1,26 @@
 import Link from "next/link"
+import Head from "next/head";
 
 const FancyBanner = () => {
    return (
-      <div className="fancy-banner-two position-relative z-1 pt-90 lg-pt-70 pb-90 lg-pb-70 mt-40">
-         <div className="container">
-            <div className="row align-items-center">
-               <div className="col-lg-8 m-auto text-center">
-                  <div className="title-one mb-50 lg-mb-40">
-                     <h2 className="text-white fs-lg">Directo a dormir.</h2>
-                     <p className="fs-24 text-white">Nosotros nos encargamos :) </p>
+      <>
+         <Head>
+            <meta name="viewport" content="width=device-width, initial-scale=1" />
+         </Head>
+         <div className="fancy-banner-two position-relative z-1 pt-90 lg-pt-70 pb-90 lg-pb-70 mt-40">
+            <div className="container">
+               <div className="row align-items-center">
+                  <div className="col-lg-8 m-auto text-center">
+                     <div className="title-one mb-50 lg-mb-40">
+                        <h2 className="text-white fs-lg">Directo a dormir.</h2>
+                        <p className="fs-24 text-white">Nosotros nos encargamos :) </p>
+                     </div>
+                     <Link href="contact" className="btn-six">Consultar</Link>
                   </div>
-                  <Link href="contact" className="btn-six">Consultar</Link>
                </div>
             </div>
          </div>
-      </div>
+      </>
    )
 }
 
