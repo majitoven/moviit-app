79f6942ba632a5fef99f0263c6fd7d06fda0ccaf
majitoven <mariajose_v96@hotmail.com>
2024-09-11 16:27:53 +0200

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,9 @@ import HomeOne from "@/components/homes/home-one";
 import Wrapper from "@/layouts/Wrapper";
 
 export const metadata = {
-  title: "Moviit Madrid",
+  title: "Moviit Madrid - Encuentra tu apartamento ideal en Madrid",
+  description: "Moviit Madrid te ayuda a encontrar el apartamento perfecto en la capital. Descubre opciones adaptadas a tus necesidades.",
+  keywords: "inmobiliaria Madrid, buscar piso Madrid, apartamentos Madrid, alquiler Madrid, moviit",
 };
 const index = () => {
   return (
